94daf2b9535a9c2d653b825cbf033a3e4b77e023
José Ignacio Guerrero VInueza <josevinguerrero@gmail.com>
2020-07-21 23:33:43 -0500

    Add Setup scripts

---
+++ bin/hello.js
@@ -0,0 +1,22 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const chalk = require('chalk');
+const pkg = require('../package.json');
+
+console.log(`
+${chalk.green('Hey there! 👋')}
+Thanks for giving the ${chalk.bold(pkg.name)} a try. 🎉
+To get you going really quickly this project includes a setup step.
+${chalk.yellow.bold('yarn run setup')} automates the following steps for you:
+  - creates a .env file with you contentful spaceId and Access Token
+  - imports ${chalk.green('a predefined content model')}
+When this is done run:
+${chalk.yellow(
+  'yarn run dev'
+)} to start a development environment at ${chalk.green('localhost:8000')}
+or
+${chalk.yellow(
+  'yarn run build'
+)} to create a production ready static site in ${chalk.green('./public')}
+For further information check the readme of the project
+(https://github.com/joseguerrerov/gatsby-starter-contentful-multi-locale)
+`);
